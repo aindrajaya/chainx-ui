4f81e6552476ad119186b44f049e1429bba27ec9
aindrajaya <arista.indrajaya@gmail.com>
2025-02-18 09:05:15 +0700

    dev: Implement base code for next.js v14

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next'
+import HelloComponent from '../components/HelloComponent'
+import Home from '../components/landing-page'
+import HomeAtomic from '../components/landingpage'
+ 
+export const metadata: Metadata = {
+  title: 'Chainx - AI Smart Contract Security Scanner',
+}
+ 
+export default function Page() {
+  return (
+    <div className=''>
+      <HomeAtomic />
+    </div>
+  )
+}

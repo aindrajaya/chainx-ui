22656d17a87f2d6af0819a29135b1b9b3409c509
Arista indrajaya <arista.indrajaya@gmail.com>
2025-02-21 15:51:23 +0700

    dev: Root header and footer implementation

---
+++ components/Footer.tsx
@@ -0,0 +1,33 @@
+export default function Footer() {
+    return (
+      <footer className="bg-white border-t border-gray-200 py-4">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-gray-500 ">© 2023 ChainX. All rights reserved.</p>
+            <div className="flex space-x-6">
+              <a
+                href="#"
+                className="text-sm text-gray-500  hover:text-gray-900"
+              >
+                Privacy Policy
+              </a>
+              <a
+                href="#"
+                className="text-sm text-gray-500  hover:text-gray-900"
+              >
+                Terms of Service
+              </a>
+              <a
+                href="#"
+                className="text-sm text-gray-500  hover:text-gray-900"
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+  
+  

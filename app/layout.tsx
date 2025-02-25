d813c0d709d40a15db09447bb14337800dbe74ba
Arista indrajaya <arista.indrajaya@gmail.com>
2025-02-25 15:45:11 +0700

    dev-ui: Add analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from 'react'
 import type { Metadata } from 'next'
 import {Inter} from "next/font/google"
 import './globals.css'
+import {Analytics} from "@vercel/analytics/next"
 
 const inter = Inter({subsets: ["latin"]})
  
@@ -28,6 +29,7 @@ export default function RootLayout({
         </head>
         <body className={inter.className}>
           {children}
+          <Analytics />
         </body>
       </html>
     )

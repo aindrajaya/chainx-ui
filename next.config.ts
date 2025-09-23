95b801f4bf1ff737fea5d80137e9e84569c06fac
Arista Indrajaya <arista.indrajaya@gmail.com>
2025-09-23 14:42:17 +0700

    fix the route manifest error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -23,15 +20,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  },
-   eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
   },
 };

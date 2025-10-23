43e62f99c189adad8bb9e19906029b892db78d33
Arista Indrajaya <arista.indrajaya@gmail.com>
2025-10-23 09:52:09 +0700

    feat: add templates data and create TemplatesPage component for displaying template options

---
+++ app/templatesData.ts
@@ -0,0 +1,36 @@
+import { Template } from './types';
+
+export const TEMPLATES: Template[] = [
+  {
+    id: 'pres-1',
+    title: 'Quarterly Sales Review for Remote Teams',
+    category: 'Presentation',
+    views: 0,
+    imageUrl: 'https://i.imgur.com/2mV2oV6.png',
+    tags: [],
+  },
+  {
+    id: 'deck-1',
+    title: 'SaaSify Seed Growth Pitch for Investors',
+    category: 'Pitch Deck',
+    views: 0,
+    imageUrl: 'https://i.imgur.com/k2p8Yik.png',
+    tags: ['PRO'],
+  },
+  {
+    id: 'prop-1',
+    title: 'ROI-Focused IT Consulting Proposal',
+    category: 'Proposal',
+    views: 0,
+    imageUrl: 'https://i.imgur.com/mJNxSAy.png',
+    tags: ['PRO'],
+  },
+  {
+    id: 'cont-1',
+    title: 'Partnership Agreement - Revenue Share & IP',
+    category: 'Contract',
+    views: 0,
+    imageUrl: 'https://i.imgur.com/x077R29.png',
+    tags: [],
+  },
+];
